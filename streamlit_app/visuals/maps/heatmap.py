44e8551e53ebfdd4825e9c7883d15e2acc41e82c
Joshua Seek <joshuaseek@gmail.com>
2025-02-09 06:55:54 -0800

    update heatmap radius

--- streamlit_app/visuals/maps/heatmap.py
+++ streamlit_app/visuals/maps/heatmap.py
@@ -5,7 +5,7 @@ def render_heatmap(issue_mapping_df):
     radius_value = st.slider(
         "Heatmap Radius", 
         min_value=1, max_value=50, 
-        value=10, step=1
+        value=2, step=1
     )
 
     fig = go.Figure(go.Densitymapbox(
