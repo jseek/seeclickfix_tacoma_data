e477deb8aa84c200aec660063f11ab9a3d2db64b
Joshua Seek <joshuaseek@gmail.com>
2025-02-09 07:45:04 -0800

    rm init

--- streamlit_app/visuals/__init__.py
+++ streamlit_app/visuals/__init__.py
@@ -9,4 +9,3 @@ from .equity_issues_analysis import display_equity_issues_analysis
 from .equity_map import display_equity_map
 from .about_311_impact import display_311_impact
 from .issue_data_table import issue_data_table
-from .council_district_review import display_council_map
